b8d5915770e617cb08a24785965d2a5c067bca49
PBrockmann <pjbrockmann@gmail.com>
2020-01-12 10:07:43 -0500

    Revised Plan forward

--- loadDataIntoPickle.py
+++ loadDataIntoPickle.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Filename: stockPortfolio
-    Description: Script
+"""Filename: loadDataIntoPickle.py
+    Description: Script is to help format data to load into Pickle
     Author: Paul Brockmann
 """
 
 import requests
 import csv
 import pickle
-
 
